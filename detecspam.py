21980f901aa7761e3b92aa95eb9de6f89b7bf826
HELAL ALI Misu <michou.helal@gmail.com>
2016-02-25 09:42:14 +0100

    add model builder

---
+++ detecspam.py
@@ -0,0 +1,79 @@
+#import matplotlib.pyplot as plt
+import csv
+from textblob import TextBlob
+import pandas
+import sklearn
+import cPickle
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.svm import SVC, LinearSVC
+from sklearn.metrics import classification_report, f1_score, accuracy_score, confusion_matrix
+from sklearn.pipeline import Pipeline
+from sklearn.grid_search import GridSearchCV
+from sklearn.cross_validation import StratifiedKFold, cross_val_score, train_test_split 
+from sklearn.tree import DecisionTreeClassifier 
+from sklearn.learning_curve import learning_curve
+
+########### Funciton #################
+def split_into_tokens(message):
+    message = unicode(message, 'utf8')  # convert bytes into proper unicode
+    return TextBlob(message).words
+def split_into_lemmas(message):
+    message = unicode(message, 'utf8').lower()
+    words = TextBlob(message).words
+    # for each word, take its "base form" = lemma 
+    return [word.lemma for word in words]
+######################################
+from optparse import OptionParser
+parser = OptionParser()
+parser.add_option("-f", "--file", dest="filename",help="Train data file  [label \t message]",default='./data/SMSSpamCollection')
+#parser.add_option("-q", "--quiet",action="store_false", dest="verbose", default=True,help="don't print status messages to stdout")
+
+(options, args) = parser.parse_args()
+
+FILE_TRAIN = options.filename
+
+messages = pandas.read_csv(FILE_TRAIN, sep='\t', quoting=csv.QUOTE_NONE,names=["label", "message"])
+bow_transformer = CountVectorizer(analyzer=split_into_lemmas).fit(messages['message'])
+messages_bow = bow_transformer.transform(messages['message'])
+tfidf_transformer = TfidfTransformer().fit(messages_bow)
+messages_tfidf = tfidf_transformer.transform(messages_bow)
+spam_detector = MultinomialNB().fit(messages_tfidf, messages['label'])
+all_predictions = spam_detector.predict(messages_tfidf)
+print classification_report(messages['label'], all_predictions)
+msg_train, msg_test, label_train, label_test = train_test_split(messages['message'], messages['label'], test_size=0.2)
+pipeline_svm = Pipeline([
+    ('bow', CountVectorizer(analyzer=split_into_lemmas)),
+    ('tfidf', TfidfTransformer()),
+    ('classifier', SVC()),  # <== change here
+])
+
+# pipeline parameters to automatically explore and tune
+param_svm = [
+  {'classifier__C': [1, 10, 100, 1000], 'classifier__kernel': ['linear']},
+  {'classifier__C': [1, 10, 100, 1000], 'classifier__gamma': [0.001, 0.0001], 'classifier__kernel': ['rbf']},
+]
+
+grid_svm = GridSearchCV(
+    pipeline_svm,  # pipeline from above
+    param_grid=param_svm,  # parameters to tune via cross validation
+    refit=True,  # fit using all data, on the best detected classifier
+    n_jobs=-1,  # number of cores to use for parallelization; -1 for "all cores"
+    scoring='accuracy',  # what score are we optimizing?
+    cv=StratifiedKFold(label_train, n_folds=5),  # what type of cross validation to use
+)
+
+svm_detector = grid_svm.fit(msg_train, label_train) # find the best combination from param_svm
+print confusion_matrix(label_test, svm_detector.predict(msg_test))
+print classification_report(label_test, svm_detector.predict(msg_test))
+
+# store the spam detector to disk after training
+with open('sms_spam_detector.pkl', 'wb') as fout:
+    cPickle.dump(svm_detector, fout)
+
+# ...and load it back, whenever needed, possibly on a different machine
+svm_detector_reloaded = cPickle.load(open('sms_spam_detector.pkl'))
+message4 = messages['message'][3]
+print 'before:', svm_detector.predict([message4])[0]
+print 'after:', svm_detector_reloaded.predict([message4])[0]
