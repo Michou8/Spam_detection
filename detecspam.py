34c22b07d4e11d7009cda44244f69f86b71dd599
Misu Helal <mhelal@giraud-sisyphe.vm.vdi.s1.p.fti.net>
2016-06-13 11:02:35 +0200

    remove svm detector

--- detecspam.py
+++ detecspam.py
@@ -74,47 +74,8 @@ all_predictions = nb.predict(messages_tfidf)
 msg_train, msg_test, label_train, label_test = train_test_split(messages['message'], messages['label'], test_size=0.3)
 
 print classification_report(messages['label'], all_predictions)
-#print confusion_matrix(label_test, svm_detector.predict(msg_test))
-#print classification_report(label_test, svm_detector.predict(msg_test))
-#raw_input()
-# store the spam detector to disk after training
-#with open('sms_spam_detector.pkl', 'wb') as fout:
-#    cPickle.dump(svm_detector, fout)
-
-
-pipeline_svm = Pipeline([
-    ('bow', CountVectorizer(decode_error='replace',ngram_range=(1,2))),
-    ('tfidf', TfidfTransformer()),
-    ('classifier', SVC()),  # <== change here
-])
-
-# pipeline parameters to automatically explore and tune
-param_svm = [
-  {'classifier__C': [1, 10, 100, 1000], 'classifier__kernel': ['linear']},
-  {'classifier__C': [1, 10, 100, 1000], 'classifier__gamma': [0.001, 0.0001], 'classifier__kernel': ['rbf']},
-]
-
-grid_svm = GridSearchCV(
-    pipeline_svm,  # pipeline from above
-    param_grid=param_svm,  # parameters to tune via cross validation
-    refit=True,  # fit using all data, on the best detected classifier
-    n_jobs=-1,  # number of cores to use for parallelization; -1 for "all cores"
-    scoring='accuracy',  # what score are we optimizing?
-    cv=StratifiedKFold(label_train, n_folds=5),  # what type of cross validation to use
-)
-
-svm_detector = grid_svm.fit(msg_train, label_train) # find the best combination from param_svm
-print confusion_matrix(label_test, svm_detector.predict(msg_test))
-print classification_report(label_test, svm_detector.predict(msg_test))
 
 # store the spam detector to disk after training
-with open('sms_spam_detector.pkl', 'wb') as fout:
-    cPickle.dump(svm_detector, fout)
+with open('nb_model.pkl', 'wb') as fout:
+    cPickle.dump(nb, fout)
 
-# ...and load it back, whenever needed, possibly on a different machine
-"""svm_detector_reloaded = cPickle.load(open('sms_spam_detector.pkl'))
-message4 = messages['message'][3]
-print 'before:', svm_detector.predict([message4])[0]
-print 'after:', svm_detector_reloaded.predict([message4])[0]
-"""
- 
