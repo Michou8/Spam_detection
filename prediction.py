5fce7247c423020dc7bcb65ed824369dc282a2bc
HELAL ALI Misu <michou.helal@gmail.com>
2016-06-21 15:31:32 +0200

    re-define the architecture

--- prediction.py
+++ prediction.py
@@ -1,4 +1,5 @@
 import csv
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from textblob import TextBlob
 import cPickle
 
@@ -15,12 +16,19 @@ def split_into_lemmas(message):
 from optparse import OptionParser
 parser = OptionParser()
 parser.add_option("-m", "--msg", dest="message",help="Message to identify if ham or spam",default='SIX chances to win CASH! From 100 to 20,000 pounds txt> CSH11 and send to 87575. Cost 150p/day, 6days, 16+ TsandCs apply Reply HL 4 info')
+parser.add_option("-t", "--train", dest="trainning",help="Train file path",default='nb_model.pkl')
 (options, args) = parser.parse_args()
 
 MESSAGE_ = options.message
-model = open('sms_spam_detector.pkl')
-#svm_detector_reloaded = cPickle.load(open('sms_spam_detector.pkl'))
+FILE_TRAIN_LOCATION = options.trainning
+model = open(FILE_TRAIN_LOCATION)
 svm_detector_reloaded = cPickle.load(model)
 model.close()
 message = MESSAGE_ 
-print 'Prediction:\t', svm_detector_reloaded.predict([message])[0]
+bow_transformer = CountVectorizer(analyzer=split_into_lemmas).fit(message)
+messages_bow = bow_transformer.transform(message)
+tfidf_transformer = TfidfTransformer().fit(messages_bow)
+messages_tfidf = tfidf_transformer.transform(messages_bow)
+
+print svm_detector_reloaded.predict(messages_tfidf)
+print 'Prediction:\t', svm_detector_reloaded.predict([messages_tfidf])[0]
--- trainning.py
+++ trainning.py
@@ -5,6 +5,7 @@ import pandas
 import sklearn
 import cPickle
 import numpy as np
+import json
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.svm import SVC, LinearSVC
@@ -30,10 +31,12 @@ from optparse import OptionParser
 parser = OptionParser()
 parser.add_option("-f", "--file", dest="filename",help="Train data file  [label \t message]",default='./data/SMSSpamCollection')
 #parser.add_option("-q", "--quiet",action="store_false", dest="verbose", default=True,help="don't print status messages to stdout")
-
+parser.add_option("-t", "--train", dest="trainning",help="Train file path",default='nb_model.pkl')
 (options, args) = parser.parse_args()
 
 FILE_TRAIN = options.filename
+FILE_TRAIN_LOCATION = options.trainning
+FILE_VECTOR = 'cvector.json'
 ################
 # You can connect to sql database
 """df = psql.read_sql(('select "Timestamp","Value" from "MyTable" '
@@ -43,15 +46,50 @@ FILE_TRAIN = options.filename
 """
 messages = pandas.read_csv(FILE_TRAIN, sep='\t', quoting=csv.QUOTE_NONE,names=["label", "message"])
 
+def cCounterWords(messages,label,model={}):
+	c = 0
+	for message in messages:
+		message = split_into_lemmas(message)
+		#data = []
+		tmp = {}
+		for word in message:
+			if word not in tmp:
+				tmp[word] = 1
+			else:
+				tmp[word] += 1
+			"""if word not in model:
+				model[word] = {'_df':0.0}
+				model[word]['_counter'] = 1.0
+			else:
+				model[word]['_counter'] += 1.0
+		for word in tmp:
+			model[word]['_df'] += 1"""
 
-bow_transformer = CountVectorizer(analyzer=split_into_lemmas).fit(messages['message'])
-messages_bow = bow_transformer.transform(messages['message'])
-tfidf_transformer = TfidfTransformer().fit(messages_bow)
-messages_tfidf = tfidf_transformer.transform(messages_bow)
+		#data.append(tmp)
+		if '_Xdata' not in model:
+			model['_Xdata'] = []
+			model['_Xlabel'] = []
+		model['_Xdata'].append(tmp)
+		model['_Xlabel'].append(label[c])
+		c += 1
+	with open(FILE_VECTOR,'wb') as f:
+		json.dump(model,f)
+	return model
+model = {}
+if FILE_VECTOR in  os.listdir('./'):
+	model = model
+model = cCounterWords(messages['message'],messages['label'],model=model)
+
+
+from sklearn.feature_extraction import DictVectorizer
+dt = DictVectorizer()
+message_bow = dt.fit_transform(model['_Xdata'])
+tfidf_transformer = TfidfTransformer().fit(message_bow)
+messages_tfidf = tfidf_transformer.transform(message_bow)
 
 classe = ['ham', 'spam']
 
-if 'nb_model.pkl' not in os.listdir("./"):
+if FILE_TRAIN_LOCATION not in os.listdir("./"):
 	print 'First trainning'
 	nb = MultinomialNB()
 	classe = ['ham', 'spam']
@@ -60,16 +98,16 @@ if 'nb_model.pkl' not in os.listdir("./"):
 	msg_train, msg_test, label_train, label_test = train_test_split(messages['message'], messages['label'], test_size=0.3)
 	print classification_report(messages['label'], all_predictions)
 	# store the spam detector to disk after training
-	with open('nb_model.pkl', 'wb') as fout:
+	with open(FILE_TRAIN_LOCATION, 'wb') as fout:
 	    cPickle.dump(nb, fout)
 else:
 	print "Training with partial_fit"
-	with open('nb_model.pkl','rb') as f:
+	with open(FILE_TRAIN_LOCATION,'rb') as f:
 		nb = cPickle.load(f)
 	nb.partial_fit(messages_tfidf, messages['label'],classes=classe)
         all_predictions = nb.predict(messages_tfidf)
         print classification_report(messages['label'], all_predictions)
         # store the spam detector to disk after training
-        with open('nb_model.pkl', 'wb') as fout:
+        with open(FILE_TRAIN_LOCATION, 'wb') as fout:
             cPickle.dump(nb, fout)
 
